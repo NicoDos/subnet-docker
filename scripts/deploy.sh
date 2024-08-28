a12df1a9b72d25b12826f51998eb901ca5f19419
Nicolas Sandron <nicolas.sandron@gmail.com>
2024-08-28 18:31:40 +0100

    docker build with linux/amd64

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Building docker image..."
-docker build -t graphite . &&
+docker build --no-cache --platform="linux/amd64" -t graphite . &&
 
 echo "Uploading docker image..."
 docker save graphite | bzip2 | pv | ssh vps 'bunzip2 | docker load'
